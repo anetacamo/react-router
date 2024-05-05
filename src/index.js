9485007bae8ca38e6f39ffbbb3a4cf276194e9a4
Aneta <anc@praqma.net>
2024-05-05 20:07:01 +0200

    removed file

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import "./global.css";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
